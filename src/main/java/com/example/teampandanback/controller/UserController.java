1890f994662e27223fef7a908ae04a19d88f98b9
HundredCleanWater <maxmp9904@gmail.com>
2021-11-30 00:09:16 +0900

    first

--- src/main/java/com/example/teampandanback/controller/UserController.java
+++ src/main/java/com/example/teampandanback/controller/UserController.java
@@ -8,30 +8,59 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-
-
+/*
+UserController를 대표하는 최상단 타이틀 영역에 표시 될 값을 세팅
+Swagger 라이브러리 어노테이션
+-REST API 개발 시 문서를 자동으로 만들어주는 프레임워크
+-API에 대한 매뉴얼 자동 생성
+-간단한 설정으로 프로젝트에 지정한 URL들을 HTML 화면으로 확인
+ */
 @Api(tags = {"로그인"})
-// Swagger 라이브러리 어노테이션
-// Swagger란?
-// - 문서 자동화 툴은 간단한 설정만으로도 테스트 가능한 Web UI를 지원하여 API 를 테스트를 위해
-//   부가적으로 서드파티 프로그램을 깔 필요가 없다.
-// - 최소한의 작업을 통해 자동으로 API Document 를 만들어주므로
-//   클라이언트 개발자에게 문서 내용을 전달하기 위해 추가 작업을 하지 않아도 된다.
-// Swagger API 문서 자동화 방법 => https://daddyprogrammer.org/post/313/swagger-api-doc/
 
 
+/*
+@RestController
+Json형태로 객체 데이터를 반환/ 메소드마다 ResponseBody를 입력 해 줄 필요가 없음
+1.Client는 URL형식으로 웹 서비스에 요청
+2.Mapping되는 Handler와 그 Type을 찾는 DispatcherServlet이 요청을 인터셉트
+3.RestController는 해당 요청을 처리하고 데이터를 반환
+
+@RequiredArgsConstructor
+초기화 되지 않은 final 필드나, @NonNull이 붙은 필드에 대해 생성자를 생성
+의존성 주입 편의성을 위해서 사용
+ */
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
+/*
+private final을 선언하면 생성자를 통해 값을 참조 할 수 있다.
+재할당하지 못하며, 해당 필드,메서드 별로 호출 할 때마다 새로이 값을 할당(인스턴스화)한다.
+ */
     private final UserService userService;
 
+    /*
+    @ApiOperation : 메소드 설명, 해당 Controller안의 method의 설명이나 설정을 추가 할 수 있다.
+    @GetMapping : Get Method 통신, user/kakao/callback과 매핑된다.
+    -> user/kakao/callback에 매핑된 API에 대한 설명으로 "카카오 소셜 로그인"이 들어간다.
+    메소드 구조 : public 리턴자료형 메소드명 (입력자료형1 입력변수 1 ){... return 리턴값}
+    @RequestParam("가져 올 데이터 이름")[데이터타입][가져온 데이터를 담을 변수])
+    RequestParm: 단일 HTTP요청 파라미터를 메소드 파라미터에 넣어줌
+    code요청을 String타입으로 변환을 하고나면 code를 통해서 카카오로그인을 하고 , HeaderDto를 userService에 넘겨줌(아마 토큰을 넘길듯?)
+     */
     @ApiOperation(value = "카카오 소셜 로그인")
     @GetMapping("/user/kakao/callback")
     public HeaderDto kakaoLogin(@RequestParam(value = "code") String code) {
         return userService.kakaoLogin(code);
     }
 
+/*
+    GET 통신에서는 RequestParam을 사용하지만 POST 통신에서는 RequestBody를 사용한다.
+    Json(application/json) 형태의 HTTP Body 내용을 Java Object로 변환시켜주는 역할
+    signupRequestDto에 있는 유저네임과 패스워드를 받아 입력 된 값을 registerUser로 넘겨준다.
+    void타입이기 때문에 무언가를 반환하지 않아도 된다.
+
+ */
     @ApiOperation(value = "기본 회원가입")
     @PostMapping("/user/signup")
     public void registerUser(@RequestBody SignupRequestDto requestDto) {
--- src/main/java/com/example/teampandanback/service/UserService.java
+++ src/main/java/com/example/teampandanback/service/UserService.java
@@ -30,11 +30,21 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Optional;
-
+/*
+@RequiredArgsConstructor
+초기화 되지 않은 final 필드나, @NonNull이 붙은 필드에 대해 생성자를 생성
+의존성 주입 편의성을 위해서 사용
+@Service
+ 클래스를 스프링이 시작할 때 Bean으로 등록해 줌
+ 해당클래스가 Service클래스 임을 확실하게 증명해줌
+ */
 @RequiredArgsConstructor
 @Service
 public class UserService {
-
+    /*
+private final을 선언하면 생성자를 통해 값을 참조 할 수 있다.
+재할당하지 못하며, 해당 필드,메서드 별로 호출 할 때마다 새로이 값을 할당(인스턴스화)한다.
+     */
     private final KakaoOAuth2 kakaoOAuth2;
     private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider;
@@ -43,38 +53,48 @@ public class UserService {
     private final ProjectService projectService;
     private final NoteService noteService;
 
+    /*
+   @Value에서 값을 주입 시 $는 Properties의 값을 읽을 때 사용하고
+   #은 Spring Expression Language을 사용 할 수 있다.
+    Vlaue : Spring환경에서 Properties 정보를 가져오는 방법
+     */
     @Value("${app.auth.tokenSecret}")
     private String secretKey;
 
 
     public HeaderDto kakaoLogin(String authorizedCode) {
-        // 카카오 OAuth2 를 통해 카카오 사용자 정보 조회
+
+        /*
+        kakaOAuth2에있는 authorizedcode를 userInfo에 넣어준다.
+         */
         KakaoUserInfo userInfo = kakaoOAuth2.getUserInfo(authorizedCode);
 
-        //nullable = false
+        /*
+         userInfo에 있는 Id,이름,비밀번호를 하나씩 부른다. 비밀번호는 시크릿키를 이용해 암호화를 한다.
+         passwordEncoder 인터페이스는 패스워드를 단방향으로 변환하여 패스워드를 안전하게 저장할 수 있게 해준다.
+         인증 시 사용자가 제공한 암호와 비교해야하는 암호를 저장하는데 사용
+         */
         Long kakaoId = userInfo.getId();
         String name = userInfo.getName();
         String password = passwordEncoder.encode(kakaoId + secretKey);
 
-        //nullable = true
+
         String picture = userInfo.getPicture();
         String email = userInfo.getEmail();
 
-//        User kakaoUser = userRepository.findByKakaoId(kakaoId)
-//                .map(entity -> entity.update(name,picture)).orElse(User.builder()
-//                        .email(email)
-//                        .picture(picture)
-//                        .name(name)
-//                        .kakaoId(kakaoId)
-//                        .password(password)
-//                        .build());
 
-
+        /*
+        orElse는 해당 값이 null 이거나 null이 아니어도 실행된다.
+        kakaoUser != null : kakaoUser가 있다면
+        이름과 사진을 갱신해주고 userRepo에 저장해달라. 만약
+        kakoUser에 없다면 밑에있는것들의 정보를 전부 kakaoUser에 넣고 레포에 저장
+        빌더:가독성,유연성이 좋고 필요한 데이터만 설정가능.
+         */
         User kakaoUser = userRepository.findByKakaoId(kakaoId).orElse(null);
         if (kakaoUser != null) {
             kakaoUser.update(name, picture);
             userRepository.save(kakaoUser);
-        } else { // 새 유저
+        } else {
             kakaoUser = User.builder()
                     .email(email)
                     .picture(picture)
@@ -84,25 +104,16 @@ public class UserService {
                     .build();
             userRepository.save(kakaoUser);
 
-//            ProjectRequestDto projectRequestDto = ProjectRequestDto.builder()
-//                    .title("판단 가이드")
-//                    .detail("판단 가이드 프로젝트 입니다.")
-//                    .build();
-//            Long guideProjectId = projectService.createProject(projectRequestDto, kakaoUser).getProjectId();
-//
-//            //환영인사
-//            NoteCreateRequestDto noteCreateRequestDto = NoteCreateRequestDto.builder()
-//                    .step(Step.STORAGE.toString())
-//                    .title("안녕하세요! 판단에 오신 것을 환영합니다!")
-//                    .deadline(LocalDateTime.now().toString())
-//                    .content("안녕하세요! 판단에 오신것을 환영합니다!\n\n각각의 노트들을 확인하며 천천히 따라와주세요!")
-//                    .files(new ArrayList<FileDetailRequestDto>())
-//                    .build();
-//            noteService.createNote()
+
 
 
         }
-
+        /*
+         위에서 저장된 kakaoUser를 새로운 userDetails로 만든다.
+        SecurityContextHolder:Authentication를 담고 있는 Holder
+        Authentication는 자체가 인증된 정보이기 때문에 SecurityContextHolder가 갖고 있는 값을 통해 인증이 되었는지 아닌지 확인을 할 수 있다.
+        kakaoUser의 정보들을 문자열로 변환한 후
+         */
         UserDetailsImpl userDetails = new UserDetailsImpl(kakaoUser);
         Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
         SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -111,7 +122,10 @@ public class UserService {
         headerDto.setTOKEN(jwtTokenProvider.createToken(Long.toString(kakaoUser.getUserId()), email, name, picture));
         return headerDto;
     }
-
+/*
+일련의 작업들을 하나의 단위로 처리하기 위해 Transactional을 사(매서드가 포함하고 있는 작업 중 하나라도 실패 할 경우 전체 작업을 취소)
+유저레포에서 아이디를 찾아 똑같은 아이디가 있으면 아이디가 중복됩니다 라고 띄우기
+ */
     @Transactional
     public void registerUser(SignupRequestDto requestDto) {
         String name = requestDto.getUsername();
@@ -129,7 +143,11 @@ public class UserService {
                 .build();
         userRepository.save(user);
     }
-
+    /*
+    orElseThrow : 빈 값을 처리하기위한 접근법
+    찾은 네임이 없다?? 그럼 가입이 안되어 있으니 가입되지 않은 유저!
+    그리고 만약 패스워드가 맞지않으면(매치되지 않으면) 잘못된 비밀번호라고 말해줌
+     */
     @Transactional
     public String login(SignupRequestDto requestDto) {
         User user = userRepository.findByName(requestDto.getUsername())
@@ -140,7 +158,7 @@ public class UserService {
         return jwtTokenProvider.createToken(Long.toString(user.getUserId()), "", user.getName(), "");
     }
 
-    //마지막 유저의 id
+
     @Transactional
     public Long getLastUserId() {
         User lastUser = userRepository.getLastUser().orElseThrow(
@@ -150,13 +168,17 @@ public class UserService {
         return lastUser.getUserId();
     }
 
-    // 이 유저는 얼마나 많은 프로젝트에 참여하였는가?
+
     @Transactional
     public Long getCountOfUserInvitedToProject(Long userId) {
         return userProjectMappingRepository.getCountOfUserInvitedToProject(userId);
     }
 
-    // 이 유저가 이 프로젝트에 참여 하였는가?
+    /*
+    이부분은 좀 어려운듯..? 매핑레포에 쿼리있고 뭐가 많음..
+    유저프로젝트매핑레포에서 프로젝트와 유저아이디를 찾아서 있으면 optuser에 넣어줘야하는데 null이 오면 optional로 감싼다.
+    만약 null이 아니라면 true null이면 false
+     */
     @Transactional
     public Boolean isUserInvitedToProject(Long userId, Long projectId) {
         Optional<UserProjectMapping> optUserProjectMapping = userProjectMappingRepository.findByUserIdAndProjectId(userId, projectId);
