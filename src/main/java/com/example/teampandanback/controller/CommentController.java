1865ba0cf580d857b35814f5f4e0802e68499670
HundredCleanWater <maxmp9904@gmail.com>
2021-12-02 00:16:13 +0900

    comment

--- src/main/java/com/example/teampandanback/controller/CommentController.java
+++ src/main/java/com/example/teampandanback/controller/CommentController.java
@@ -13,14 +13,41 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
+/*
+UserController를 대표하는 최상단 타이틀 영역에 표시 될 값을 세팅
+Swagger 라이브러리 어노테이션
+-REST API 개발 시 문서를 자동으로 만들어주는 프레임워크
+-API에 대한 매뉴얼 자동 생성
+-간단한 설정으로 프로젝트에 지정한 URL들을 HTML 화면으로 확인
+ */
 @Api(tags = {"댓글"})
+
+/*
+@RestController
+Json형태로 객체 데이터를 반환/ 메소드마다 ResponseBody를 입력 해 줄 필요가 없음
+1.Client는 URL형식으로 웹 서비스에 요청
+2.Mapping되는 Handler와 그 Type을 찾는 DispatcherServlet이 요청을 인터셉트
+3.RestController는 해당 요청을 처리하고 데이터를 반환
+
+@RequiredArgsConstructor
+초기화 되지 않은 final 필드나, @NonNull이 붙은 필드에 대해 생성자를 생성
+의존성 주입 편의성을 위해서 사용
+ */
+
 @RequiredArgsConstructor
 @RestController
 public class CommentController {
-
+    /*
+private final을 선언하면 생성자를 통해 값을 참조 할 수 있다.
+재할당하지 못하며, 해당 필드,메서드 별로 호출 할 때마다 새로이 값을 할당(인스턴스화)한다.
+ */
     private final CommentService commentService;
-
+    /*
+   @ApiOperation : 메소드 설명, 해당 Controller안의 method의 설명이나 설정을 추가 할 수 있다.
+    @PathVariable : 경로의 특정 위치 값이 고정되지 않고 달라질 때 사용하는 것
+    @AuthenticationPrincipal:Authentication(인증 정보에 대한 부분을 정의해놓은 인터페이스) + Principal("인증되는 주체의 ID")
+    @RequestBody:객체 생성 가능하지만 각 변수별로 데이터 저장은 불가능(RequestParam과 반대)
+    */
     //코멘트 작성
     @ApiOperation(value = "댓글 작성")
     @PostMapping("/api/comments/{noteId}")
--- src/main/java/com/example/teampandanback/service/CommentService.java
+++ src/main/java/com/example/teampandanback/service/CommentService.java
@@ -26,7 +26,14 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
+/*
+@RequiredArgsConstructor
+초기화 되지 않은 final 필드나, @NonNull이 붙은 필드에 대해 생성자를 생성
+의존성 주입 편의성을 위해서 사용
+@Service
+ 클래스를 스프링이 시작할 때 Bean으로 등록해 줌
+ 해당클래스가 Service클래스 임을 확실하게 증명해줌
+ */
 @RequiredArgsConstructor
 @Service
 public class CommentService {
@@ -36,6 +43,10 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final UserProjectMappingRepository userProjectMappingRepository;
 
+
+    /*
+    orElseThrow -> 만약 값이 없으면 예외적으로 밑에 문구를 나타냄
+     */
     public CommentCreateResponseDto createComment(Long noteId, User currentUser, CommentCreateRequestDto commentCreateRequestDto) {
         User user = userRepository.findById(currentUser.getUserId()).orElseThrow(
                 () -> new ApiRequestException("등록되지 않은 유저의 접근입니다.")
@@ -44,18 +55,29 @@ public class CommentService {
         Note note = noteRepository.findById(noteId).orElseThrow(
                 () -> new ApiRequestException("생성되지 않은 노트입니다.")
         );
-
+        /*
+        Optional -> Null인지 아닌지 확실 한 수 없는 객체를 담고 있을 경우 작성
+        ofNullable은 null이 넘어 올 경우 Optional.empty()와 동일하게 비어 있는 Optional 객체를 얻어옴.
+        해당객체가 null인지 아닌지 모르기때문에 empty대신 ofNullable을 사용함.
+        즉, project가 있으면 그냥 꺼내면 되지만 만약 Null이면 비어있는 Optional 객체를 얻어와 연결된 프로젝트가 없다고 뜸
+         */
         //쿼리
         Project connectedProject = Optional.ofNullable(note.getProject()).orElseThrow(
                 () -> new ApiRequestException("연결된 프로젝트가 없습니다.")
         );
 
+        /*
+        User와 Project에서 user와 connedctedProject를 찾아주고 만약 없으면 문구 반환하기
+         */
         //쿼리
         UserProjectMapping userProjectMapping = userProjectMappingRepository.findByUserAndProject(user, connectedProject)
                 .orElseThrow(
                         () -> new ApiRequestException("user와 project mapping을 찾지 못했습니다.")
                 );
 
+        /*
+
+         */
         Comment newComment = Comment.builder()
                 .user(user)
                 .note(note)
@@ -81,6 +103,12 @@ public class CommentService {
         );
 
         //쿼리
+        /*
+              Optional -> Null인지 아닌지 확실 한 수 없는 객체를 담고 있을 경우 작성
+        ofNulable은 null이 넘어 올 경우 Optional.empty()와 동일하게 비어 있는 Optional 객체를 얻어옴.
+        해당객체가 null인지 아닌지 모르기때문에 empty대신 ofNullable을 사용함.
+        즉, project가 있으면 그냥 꺼내면 되지만 만약 Null이면 비어있는 Optional 객체를 얻어와 연결된 프로젝트가 없다고 뜸
+         */
         Project connectedProject = Optional.ofNullable(note.getProject()).orElseThrow(
                 () -> new ApiRequestException("연결된 프로젝트가 없습니다.")
         );
@@ -90,7 +118,12 @@ public class CommentService {
                 .orElseThrow(
                         () -> new ApiRequestException("user와 project mapping을 찾지 못했습니다.")
                 );
-
+        /*
+        stream : 람다함수형식으로 배열들을 가공 ( for , foreach문을 사용안해도 됨.. 이거쓰는사람 부러움..)
+        map: 요소들의 특정 조건에 해당하는 값으로 변환해줌
+        collect : 가공이 끝난 요소들을 리턴해줄 결과값을 넣어 줌 .
+        sorted : 요소들을 정렬-> getCreatedAt: 댓글생성 시간순으로 정렬하는듯 .
+         */
         List<Comment> commentList = commentRepository.findByNoteId(noteId);
         List<Comment> commentListSortedByCreatedAt = commentList.stream().sorted(Comparator.comparing(Comment::getCreatedAt)).collect(Collectors.toList());
         List<CommentReadEachResponseDto> commentReadEachResponseDtoList =
@@ -106,6 +139,11 @@ public class CommentService {
     }
 
     // 댓글 수정
+    /*
+    .filter : 특정조건으로 스트림의 컨텐츠를 필터링 하는 것.현재 로그인 되어 있는 Id와 댓글 작성한 Id가 같은지 필터링을 거치는 듯?
+    .map : 어떻게 변경 시킬것인가? -> 코멘트업데이트리퀘스트디티오에 업데이트 한다!
+    .orElseThrow : 아이디가 다르면 이제 저 문구을 보여줘
+     */
     @Transactional
     public CommentUpdateResponseDto updateComment(Long commentId, User currentUser, CommentUpdateRequestDto commentUpdateRequestDto) {
 
